3777e2996f8720c1527d5f62c32a9e361b43b3b9
divyasingh1 <er.divyasingh1@gmail.com>
2021-06-19 16:25:24 +0530

    adding remove manufacturer function

--- prada_contract.sol
+++ prada_contract.sol
@@ -20,6 +20,8 @@ contract  Prada_product {
    event RejectTransfer(address from, address to, string uuid, string message);
    event AddManufacturer(address from,  string message);
    event RejectAddManufacturar(address from, string message);
+   event RejectDeleteManufacturar(address from, string message);
+   event DeleteManufacturar(address from, string message);
    
    mapping(string  => Product) private productStore;
    
@@ -42,43 +44,44 @@ contract  Prada_product {
          require(manufacturer[msg.sender].initialized == true);
          _; 
     }
-    
+
 
    function addManufacturer(address _manufacturer , string license) onlyOwner public {
          if(manufacturer[_manufacturer].initialized){
-             emit RejectAddManufacturar(msg.sender, "Manufecturar already added");
-             revert("Manufecturar already added");
+             emit RejectAddManufacturar(msg.sender, "Manufacturar already added");
+             revert("Manufacturar already added");
              return;
          }
          
-        //  if(manufacturer[_manufacturer].license == license){
-        //      emit RejectAddManufacturar(msg.sender, "Manufecturar already added");
-        //      revert("Manufecturar already added");
-        //      return  ;
-        //  }
-      
          manufacturer[_manufacturer] = Manufacturer(license, true);
          emit AddManufacturer(msg.sender, "Manufacturer added successfully");
    }
    
-//   function removeManufacturer(address _manufacturer , string name, string official_address){
-    
-//   }
+  function removeManufacturer(address _manufacturer) onlyOwner public {
+      if(manufacturer[_manufacturer].initialized){
+          manufacturer[_manufacturer].initialized =  false;
+          emit DeleteManufacturar(msg.sender, "Manufecturar deleted");
+          return;
+       }
+       emit RejectDeleteManufacturar(msg.sender, "Manufecturar already deleted");
+       revert("Manufacturar already deleted");
+       return;
+  }
    
     
-   function createProduct(string name, string description, string uuid, string manufacturer) onlyManufacturar  public{
+   function createProduct(string name, string description, string uuid, string manufacturerName) onlyManufacturar  public{
       if(productStore[uuid].initialized) {
         emit RejectCreate(msg.sender, uuid, "Asset with this UUID already exists.");
         revert("Asset with this UUID already exists");
         return;
       }
 
-      productStore[uuid] = Product(name, description, manufacturer, true );
+      productStore[uuid] = Product(name, description, manufacturerName, true );
       walletStore[msg.sender][uuid] = true;
-      emit ProductCreate(msg.sender, uuid, manufacturer);
+      emit ProductCreate(msg.sender, uuid, manufacturerName);
    }
     
-    function transferProduct(address to, string uuid) public {
+    function transferProduct(address to, string uuid)  public {
         if(!productStore[uuid].initialized) {
           emit RejectTransfer(msg.sender, to, uuid, "No asset with this UUID exists");
           revert("No asset with this UUID exists");
@@ -90,7 +93,6 @@ contract  Prada_product {
            revert("Sender does not own this asset.");
            return;
         }
-
         
         walletStore[msg.sender][uuid] = false;
         walletStore[to][uuid] = true;
@@ -105,8 +107,8 @@ contract  Prada_product {
        return (manufacturer[_address].license, manufacturer[_address].initialized);
     }
     
-    function isOwnerOf(address owner, string uuid) public constant returns (bool) {
-         if(walletStore[owner][uuid]) {
+    function isOwnerOf(address _owner, string uuid) public constant returns (bool) {
+         if(walletStore[_owner][uuid]) {
            return true;
           }
          revert("Product does not exist");
