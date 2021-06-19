b37d807e45cf703a12ecf6a19db69297c204821b
divyasingh1 <er.divyasingh1@gmail.com>
2021-06-19 15:20:11 +0530

    adding modifiers and add Manufacturer functiion

--- prada_contract.sol
+++ prada_contract.sol
@@ -3,33 +3,77 @@ pragma solidity ^0.4.24;
 contract  Prada_product { 
     
    struct Product {
-    string name;
-    string description;
-    string manufacturer;
-    bool initialized; 
-}
+     string name;
+     string description;
+     string manufacturer;
+     bool initialized;
+   }
+   
+   struct Manufacturer {
+     string license;
+     bool initialized;
+   }
    
    event ProductCreate(address account, string uuid, string manufacturer);
    event ProductTransfer(address from, address to, string uuid);
    event RejectCreate(address account, string uuid, string message);
    event RejectTransfer(address from, address to, string uuid, string message);
+   event AddManufacturer(address from,  string message);
+   event RejectAddManufacturar(address from, string message);
    
    mapping(string  => Product) private productStore;
    
    mapping(address => mapping(string => bool)) private walletStore;
    
+   mapping(address => Manufacturer) private manufacturer;
+   
+   address owner;
+   
    constructor () public { 
+       owner=msg.sender; 
    }
+  
+    modifier onlyOwner {
+       require(msg.sender == owner);
+       _; 
+    }
     
+    modifier onlyManufacturar {
+         require(manufacturer[msg.sender].initialized == true);
+         _; 
+    }
     
-   function createProduct(string name, string description, string uuid, string manufacturer) public{
+
+   function addManufacturer(address _manufacturer , string license) onlyOwner public {
+         if(manufacturer[_manufacturer].initialized){
+             emit RejectAddManufacturar(msg.sender, "Manufecturar already added");
+             revert("Manufecturar already added");
+             return;
+         }
+         
+        //  if(manufacturer[_manufacturer].license == license){
+        //      emit RejectAddManufacturar(msg.sender, "Manufecturar already added");
+        //      revert("Manufecturar already added");
+        //      return  ;
+        //  }
+      
+         manufacturer[_manufacturer] = Manufacturer(license, true);
+         emit AddManufacturer(msg.sender, "Manufacturer added successfully");
+   }
+   
+//   function removeManufacturer(address _manufacturer , string name, string official_address){
+    
+//   }
+   
+    
+   function createProduct(string name, string description, string uuid, string manufacturer) onlyManufacturar  public{
       if(productStore[uuid].initialized) {
         emit RejectCreate(msg.sender, uuid, "Asset with this UUID already exists.");
         revert("Asset with this UUID already exists");
         return;
       }
 
-      productStore[uuid] = Product(name, description, manufacturer, true);
+      productStore[uuid] = Product(name, description, manufacturer, true );
       walletStore[msg.sender][uuid] = true;
       emit ProductCreate(msg.sender, uuid, manufacturer);
    }
@@ -47,13 +91,18 @@ contract  Prada_product {
            return;
         }
 
+        
         walletStore[msg.sender][uuid] = false;
         walletStore[to][uuid] = true;
         emit ProductTransfer(msg.sender,  to, uuid);
     }
     
     function getProductByProductId(string uuid) public constant returns (string, string, string, bool) {
-       return (productStore[uuid].name, productStore[uuid].description, productStore[uuid].manufacturer, productStore[uuid].initialized);
+       return (productStore[uuid].name, productStore[uuid].description, productStore[uuid].manufacturer, productStore[uuid].initialized );
+    }
+    
+    function getManufacturar(address _address) public constant returns (string, bool){
+       return (manufacturer[_address].license, manufacturer[_address].initialized);
     }
     
     function isOwnerOf(address owner, string uuid) public constant returns (bool) {
